4dd596737a3597daf1417e45232f99f0a97717cf
Martin Filípek <martinf@appsdevteam.com>
2025-12-10 07:31:38 +0100

    Typo repair

--- ios/Classes/SwiftFlutterTesseractOcrPlugin.swift
+++ ios/Classes/SwiftFlutterTesseractOcrPlugin.swift
@@ -2,7 +2,7 @@ import Flutter
 import UIKit
 import SwiftyTesseract
 
-struct FileSystemTessDataSource: LanguageModelDataSource {
+struct FileSystemLanguageModelDataSource: LanguageModelDataSource {
     let pathToTrainedData: String
 }
 
